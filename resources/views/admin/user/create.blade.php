531b5ebd0f28ddde93bc9a232651d7078ec9331f
Takeshi Miura <101356765+Takeshi-mi@users.noreply.github.com>
2024-06-29 23:07:44 -0300

    templates(pasta layouts) @extends @section @yield

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -1,13 +1,17 @@
+@extends('admin.layouts.app')
+@section('title', 'Criar novo usuário')
+@section('content')
 <h1>Novo usuário</h1>
 
-<form action="{{ route('users.store') }}" method="POST">
-    <!--<input type='text' name="_token" value="{{ csrf_token() }}"> --> 
-    <!-- Previne erros. Mas tem uma forma mais simples de fazer isso, com o @csrf 
-    Indica que é uma requisição válida o token. Senão dá o erro 419. O csrf é um helper do laravel que são métodos disponíveis em todo o projeto-->
-    @csrf()
-    <input type="text" name="name" placeholder="Nome" required>    
-    <input type="email" name="email" placeholder="E-mail" required>
-    <input type="password" name="password" placeholder="Senha" required>
-    <button type="submit">Enviar</button>
-    
-</form>
+    <form action="{{ route('users.store') }}" method="POST">
+        <!--<input type='text' name="_token" value="{{ csrf_token() }}"> -->
+        <!-- Previne erros. Mas tem uma forma mais simples de fazer isso, com o @csrf
+        Indica que é uma requisição válida o token. Senão dá o erro 419. O csrf é um helper do laravel que são métodos disponíveis em todo o projeto-->
+        @csrf()
+        <input type="text" name="name" placeholder="Nome" required>
+        <input type="email" name="email" placeholder="E-mail" required>
+        <input type="password" name="password" placeholder="Senha" required>
+        <button type="submit">Enviar</button>
+
+    </form>
+@endsection
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,41 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Usuários</title>
-</head>
-<body>
-    
-    <h1>Usuários</h1>
+@extends('admin.layouts.app')
+@section('title', "Listagem de Usuários")
 
-    <a href="{{ route('users.create')}}">Novo</a>
-<!-- Eu poderia passar href="/users/crete. Mas é muito melhor passar a referência pelo nome, pois se um dia eu mudar a url é mais fácil de dar manutenção no código-->
-    <table>
-        <thead>
+@section('content')
+<h1>Usuários</h1>
+<a href="{{ route('users.create')}}">Novo</a>
+<!-- Eu poderia passar href="/users/create. Mas é muito melhor passar a referência pelo nome, pois se um dia eu mudar a url é mais fácil de dar manutenção no código-->
+<table>
+    <thead>
+        <tr>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($users as $user)
+        
             <tr>
-                <th>Nome</th>
-                <th>E-mail</th>
-                <th>Ações</th>
+                <td>{{$user->name}}</td>
+                <td>{{$user->email}}</td>
+                <td></td>
             </tr>
-        </thead>
-        <tbody>
-            @forelse ($users as $user)
-            
-                <tr>
-                    <td>{{$user->name}}</td>
-                    <td>{{$user->email}}</td>
-                    <td></td>
-                </tr>
 
-            @empty <!--caso esteja vazio. Funciona por causa do forelse. Ao invés de ter usado o foreach.-->
-            <tr>
-                <td colspan="100">Nenhum usuário no banco</td>
-            </tr>
-            @endforelse
-        </tbody>
-    </table>
-    
-    {{ $users->links() }}
-</body>
-</html>
+        @empty <!--caso esteja vazio. Funciona por causa do forelse. Ao invés de ter usado o foreach.-->
+        <tr>
+            <td colspan="100">Nenhum usuário no banco</td>
+        </tr>
+        @endforelse
+    </tbody>
+</table>
+
+{{ $users->links() }}
+
+@endsection
